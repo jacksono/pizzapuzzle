0649623dcd1eca9fc5e6567e50eb09127820ddab
Jxn <jackson.onyango@gmail.com>
2017-01-24 19:10:09 +0300

    Add minimum ingrediens per slice and max cells per slice

--- pizza.py
+++ pizza.py
@@ -6,12 +6,18 @@ input = open("input.txt", "rt")
 instructions = []
 for line in input:
     instructions.append(line.strip())
+input.close()
 rows_cols = instructions[0].split(' ')[0:2] #extracting rows and columns from the file
 contents = instructions[1:]                 #extracting the pizza contents
 for i in range(int(rows_cols[0])):          #printing the picture of the pizza
     print(contents[i])
     print()
 
-input.close()
+min_ing_per_slice = instructions[0].split(' ')[2]
+max_cells_per_slice = instructions[0].split(' ')[-1]
+
+
 print('these are all the instructions:',instructions)
 print('these are all the contents: {}'.format(contents))
+print('these is the min ingredients per slice:',min_ing_per_slice)
+print('these are the max cells per slice:',max_cells_per_slice)
