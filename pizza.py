3690a12ebf868ab608f6255de8ae921e67b51440
Jxn <jackson.onyango@gmail.com>
2017-01-25 15:50:40 +0300

    Modify function to cut out one slice of any size without holes starting from anywhere

--- pizza.py
+++ pizza.py
@@ -16,17 +16,17 @@ for i in range(int(rows_cols[0])):          #printing the picture of the pizza
 min_ing_per_slice = instructions[0].split(' ')[2]
 max_cells_per_slice = instructions[0].split(' ')[-1]
 
-def one_slice(piza, size): #function to cut out one slice of any size without holes starting from col 1 but any row
-    start = [0,2] # i have used zero based coords so that the frst line is 0,0 not 1,1
-    one_slice = []
+def one_slice(piza, size, start): #function to cut out one slice of any size without holes starting from any col but any row
+    one_slice = []                # for start coords i have used zero based coords so that the frst line is 0,0 not 1,1
 
-    if size[1] > len(piza[start[1]:]) or size[0] > len(piza[0]): #check if slice size can fit in pizza
+    if size[1] > len(piza[start[1]:]) or size[0] > len(piza[0][start[0]:]): #check if slice size can fit in pizza
         return ('Size too big for this piza')
 
     else:
         new_piza = piza[start[1]:]
         for row in new_piza[0:size[1]]:
-            slice_row = row[0:size[0]]
+            new_row = row[start[0]:]
+            slice_row = new_row[0:size[0]]
             one_slice.append(slice_row)
         return one_slice
     
@@ -37,4 +37,4 @@ print('these are all the instructions:',instructions)
 print('these are all the contents: {}'.format(contents))
 print('these is the min ingredients per slice:',min_ing_per_slice)
 print('these are the max cells per slice:',max_cells_per_slice)
-print(one_slice(contents,[3,1]))
+print(one_slice(contents,[3,2], [2,1]))
