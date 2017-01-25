2ea2e30819c74ddc70d0f91b52a1823e74aaddb7
Jxn <jackson.onyango@gmail.com>
2017-01-25 15:27:50 +0300

    Add function to cut out one slice of any size without holes starting from [0,0]

--- pizza.py
+++ pizza.py
@@ -16,8 +16,23 @@ for i in range(int(rows_cols[0])):          #printing the picture of the pizza
 min_ing_per_slice = instructions[0].split(' ')[2]
 max_cells_per_slice = instructions[0].split(' ')[-1]
 
+def one_slice(piza, size): #function to cut out one slice of any size without holes starting from [0,0]
+    start = [0,0]
+    one_slice = []
+    if size[1] > len(piza) or size[0] > len(piza[0]):
+        return ('Size too big for this piza')
+
+    else:
+        for row in piza[0:size[1]]:
+            slice_row = row[0:size[0]]
+            one_slice.append(slice_row)
+        return one_slice
+    
+
+
 
 print('these are all the instructions:',instructions)
 print('these are all the contents: {}'.format(contents))
 print('these is the min ingredients per slice:',min_ing_per_slice)
 print('these are the max cells per slice:',max_cells_per_slice)
+print(one_slice(contents,[1,1]))
